236cd09f540e8c3a69c9d7e753086f0e561c8a99
Devin Gaffney <itsme@devingaffney.com>
2015-09-01 11:56:29 -0400

    updates

--- lib/network_analysis_t_step.rb
+++ lib/network_analysis_t_step.rb
@@ -35,17 +35,7 @@ class NetworkAnalysisTStep
   end
 
   def self.store_result(net)
-    month, day, year, hour = net[:end_time].split(/[- ]/)
-    end_time = net[:strftime_template].include?("H") ? Time.parse("#{year}-#{month}-#{day} #{hour}:00:00") : Time.parse("#{year}-#{month}-#{day} 00:00:00")
-    sr2 = StatResultTwo.first_or_create(hashtag: net[:hashtag], end_time: end_time, net_statistic: net[:net_statistic]||"indegree", strftime_template: net[:strftime_template])
-    sr2.tau = net[:tau].nan? ? 0 : net[:tau]
-    sr2.n = net[:n]
-    sr2.index = net[:index]
-    sr2.article_count = MediaCloudArticle.where(hashtag: net[:hashtag], :publish_date.lte => end_time).count
-    sr2.alexa_mean = AlexaRank.mean(net[:hasthag], end_time)
-    sr2.alexa_max = AlexaRank.max(net[:hasthag], end_time)
-    sr2.alexa_sum = AlexaRank.sum(net[:hasthag], end_time)
-    sr2.save!
+    StoreStatResultTwo.perform_async(net)
   end
   
   def self.run_by_analytic(hashtag, strftime_template, analytic)
