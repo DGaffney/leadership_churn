2bf963a6ad29820c64f4a13b61278d7f560f8ff8
Devin Gaffney <itsme@devingaffney.com>
2015-09-06 13:34:28 -0700

    updates

--- models/bulk_survival_analysis.rb
+++ models/bulk_survival_analysis.rb
@@ -4,4 +4,9 @@ class BulkSurvivalAnalysis
   key :timestamp, Time
   key :strftime_template, String
   key :results, Hash
+  
+  def self.survivor_roll_call(hashtag, strftime_template)
+    self.where(hashtag: hashtag, strftime_template: strftime_template).order(:timestamp).each do |survivors|
+    end
+  end
 end
