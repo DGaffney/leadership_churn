80f0b59a1e52fc7cefb7cd9ea84e8e9ab3d4c00a
Devin Gaffney <itsme@devingaffney.com>
2015-09-06 13:22:57 -0700

    updates

--- tasks/store_survival_analysis.rb
+++ tasks/store_survival_analysis.rb
@@ -3,9 +3,10 @@ class StoreSurvivalAnalysis
   sidekiq_options :queue => :store_survival
   def perform(record)
     obj = BulkSurvivalAnalysis.first_or_create(hashtag: record["hashtag"], timestamp: record["timestamp"], strftime_template: record["strftime_template"])
-    obj.results = record["results"]
+    obj.results = Hash[record["results"]]
     obj.save!
   end
 end
+
 # BulkSurvivalAnalysis.ensure_index([[:hashtag, 1], [:timestamp, 1], [:strftime_template, 1]])
 # SurvivalAnalysisRecord.ensure_index([[:screen_name, 1], [:hashtag, 1], [:timestamp, 1], [:strftime, 1], [:metric_name, 1]])
